81a87d646a317dde685eb1e767ff5256cba6232f
noxiss <kamiljakubczak@outlook.com>
2020-08-06 20:08:38 +0200

    Change tests for tag model to support tags for user

--- api/tests/test_tag_model.py
+++ api/tests/test_tag_model.py
@@ -1,13 +1,32 @@
 from django.test import TestCase
 from api.models import Tag
+from django.contrib.auth import get_user_model
+from rest_framework.test import APIClient
 
 
 class ModelTests(TestCase):
 
+
+
+    def setUp(self):
+
+        self.user = get_user_model().objects.create_user(
+            'testuser',
+            'supertest'
+        )
+        self.client = APIClient()
+
     def test_retrieve_recursd_tag_name(self):
-        tag1 = Tag.objects.create(name='Tag1')
-        tag2 = Tag.objects.create(name='Tag2', parent_tag=tag1)
-        tag3 = Tag.objects.create(name='Tag3', parent_tag=tag2)
+        tag1 = Tag.objects.create(name='Tag1',
+                                  user=self.user)
+
+        tag2 = Tag.objects.create(name='Tag2',
+                                  user=self.user,
+                                  parent_tag=tag1)
+
+        tag3 = Tag.objects.create(name='Tag3',
+                                  user=self.user,
+                                  parent_tag=tag2)
 
         expected1 = 'Tag1'
         self.assertEqual(tag1.__str__(), expected1)
