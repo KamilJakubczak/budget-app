7b59f3fa0fe80e69a3b27c6f49240a44f00b41d1
noxiss <kamiljakubczak@outlook.com>
2020-08-23 15:37:48 +0200

    Added header component

---
+++ frontend/src/components/layout/Header.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+
+export class Header extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+        <a className="navbar-brand" href="#">
+          Budget
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarColor01"
+          aria-controls="navbarColor01"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div
+          className="collapse navbar-collapse justyfy-content-end "
+          id="navbarColor01"
+        >
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="#">
+                Transactions
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Accounts
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Planning
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default Header;
